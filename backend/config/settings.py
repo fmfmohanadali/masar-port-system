2d73cce846159eb6d0c947aed681b32069a368a9
MohanadAli <102630539+fmfmohanadali@users.noreply.github.com>
2026-05-28 05:03:33 +0300

    Change database configuration to use SQLite

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -61,11 +61,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 LANGUAGE_CODE = "ar"
